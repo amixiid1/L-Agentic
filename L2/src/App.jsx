539cb1b4b8ae1c38c412cc21337923b0f064e019
amixiid1 <amixiid0@gmail.com>
2026-03-14 11:14:22 +0300

    assingment 1

---
+++ L2/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+import ContactPage from "./ContactPage";
+import HomePage from "./HomePage";
+import "./App.css";
+
+const App = () => {
+  return (
+
+      <>
+        <HomePage />
+        <ContactPage />
+      </>
+    );
+  
+};
+
+export default App;
